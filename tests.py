6299c6bad9f2cd8663fd4f525f0f0bfe97a518f6
Elena Ilina <p7f8hf4g08@yandex.com>
2022-11-27 21:02:35 +0300

    Add base tests

--- tests.py
+++ tests.py
@@ -8,7 +8,21 @@ class TestBooksCollector:
     # обязательно указывать префикс test_
     # дальше идет название метода, который тестируем add_new_book_
     # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
+    def test_add_new_book_true(self):
+        collector = BooksCollector()
+        book_name = 'Гордость и предубеждение и зомби'
+        collector.add_new_book(book_name)
+
+        assert book_name in collector.books_rating
+
+    def test_new_book_has_rating_one_true(self):
+        collector = BooksCollector()
+        book_name = 'Гордость и предубеждение и зомби'
+        collector.add_new_book(book_name)
+
+        assert collector.get_book_rating(book_name) == 1
+
+    def test_add_two_books_true(self):
         # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
 
@@ -20,5 +34,95 @@ class TestBooksCollector:
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
         assert len(collector.get_books_rating()) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_set_book_rating_true(self):
+        collector = BooksCollector()
+        book_name = 'Гордость и предубеждение и зомби'
+        book_rating = 9
+        collector.add_new_book(book_name)
+        collector.set_book_rating(book_name, book_rating)
+        assert collector.books_rating[book_name] == book_rating
+
+    def test_set_book_rating_false(self):
+        collector = BooksCollector()
+        book_name = 'Гордость и предубеждение и зомби'
+        book_rating = 11
+        collector.add_new_book(book_name)
+        collector.set_book_rating(book_name, book_rating)
+        assert collector.get_book_rating(book_name) != book_rating and collector.get_book_rating(book_name) == 1
+
+    def test_get_book_rating_true(self):
+        collector = BooksCollector()
+        book_name = 'Гордость и предубеждение и зомби'
+        book_rating = 9
+        collector.add_new_book(book_name)
+        collector.set_book_rating(book_name, book_rating)
+        assert collector.get_book_rating(book_name) == book_rating
+
+    def test_get_books_with_specific_rating_true(self):
+        collector = BooksCollector()
+        book_name_1 = 'Гордость и предубеждение и зомби'
+        book_name_2 = 'Что делать, если ваш кот хочет вас убить'
+        book_rating_1 = 9
+        book_rating_2 = 2
+        collector.add_new_book(book_name_1)
+        collector.set_book_rating(book_name_1, book_rating_1)
+        collector.add_new_book(book_name_2)
+        collector.set_book_rating(book_name_2, book_rating_2)
+        assert collector.get_books_with_specific_rating(9) == [book_name_1]
+
+    def test_get_books_rating_true(self):
+        collector = BooksCollector()
+        book_name_1 = 'Гордость и предубеждение и зомби'
+        book_name_2 = 'Что делать, если ваш кот хочет вас убить'
+        book_rating_1 = 9
+        book_rating_2 = 2
+        collector.add_new_book(book_name_1)
+        collector.set_book_rating(book_name_1, book_rating_1)
+        collector.add_new_book(book_name_2)
+        collector.set_book_rating(book_name_2, book_rating_2)
+        assert collector.get_books_rating() == {book_name_1: book_rating_1,
+                                                book_name_2: book_rating_2}
+
+    def test_add_book_in_favorites_true(self):
+        collector = BooksCollector()
+        book_name_1 = 'Гордость и предубеждение и зомби'
+        book_name_2 = 'Что делать, если ваш кот хочет вас убить'
+        book_rating_1 = 9
+        book_rating_2 = 2
+        collector.add_new_book(book_name_1)
+        collector.set_book_rating(book_name_1, book_rating_1)
+        collector.add_new_book(book_name_2)
+        collector.set_book_rating(book_name_2, book_rating_2)
+        collector.add_book_in_favorites(book_name_1)
+        assert book_name_1 in collector.favorites
+
+    def test_delete_book_from_favorites_true(self):
+        collector = BooksCollector()
+        book_name_1 = 'Гордость и предубеждение и зомби'
+        book_name_2 = 'Что делать, если ваш кот хочет вас убить'
+        book_rating_1 = 9
+        book_rating_2 = 2
+        collector.add_new_book(book_name_1)
+        collector.set_book_rating(book_name_1, book_rating_1)
+        collector.add_new_book(book_name_2)
+        collector.set_book_rating(book_name_2, book_rating_2)
+        collector.add_book_in_favorites(book_name_1)
+        collector.add_book_in_favorites(book_name_2)
+        collector.delete_book_from_favorites(book_name_1)
+        assert len(collector.favorites) == 1 and book_name_2 in collector.favorites
+
+    def test_get_list_of_favorites_books_true(self):
+        collector = BooksCollector()
+        book_name_1 = 'Гордость и предубеждение и зомби'
+        book_name_2 = 'Что делать, если ваш кот хочет вас убить'
+        book_rating_1 = 9
+        book_rating_2 = 2
+        collector.add_new_book(book_name_1)
+        collector.set_book_rating(book_name_1, book_rating_1)
+        collector.add_new_book(book_name_2)
+        collector.set_book_rating(book_name_2, book_rating_2)
+        collector.add_book_in_favorites(book_name_1)
+        collector.add_book_in_favorites(book_name_2)
+        assert len(collector.get_list_of_favorites_books()) == 2 and \
+               book_name_1 in collector.get_list_of_favorites_books() and \
+               book_name_2 in collector.get_list_of_favorites_books()
